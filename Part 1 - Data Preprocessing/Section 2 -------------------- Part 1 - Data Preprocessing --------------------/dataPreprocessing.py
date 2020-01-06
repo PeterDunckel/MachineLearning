e12d82330d0416a9c217740ab8ebcd354431e811
Peter Dunckel <peterdunckel@Peters-MacBook-Pro.local>
2020-01-05 16:46:19 -0800

    Modified Preprocessing sample to encode categorical data using labelencoder and hotencoder

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/dataPreprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/dataPreprocessing.py
@@ -16,3 +16,16 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = "mean", axis = 0)
 imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:, 1:3]) 
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+#break down first column to numerical values representing categories
+x[:,0] = labelencoder_X.fit_transform(x[:,0])
+#split first column into columns representing each piece of categorical data
+#in the first column with 1 representing that this category is selected
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x = onehotencoder.fit_transform(x).toarray()
+#break down labels into numerical values
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
