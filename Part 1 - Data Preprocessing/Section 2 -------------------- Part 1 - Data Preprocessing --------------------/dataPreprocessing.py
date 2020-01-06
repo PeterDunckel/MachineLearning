6dfefef355c0f06235ca70b4b45e9e1870d53e78
Peter Dunckel <peterdunckel@Peters-MacBook-Pro.local>
2020-01-05 17:32:52 -0800

    create data sets for training the machine learning algorithm to understand the proprocessed data and for testing the machine learning algorithm

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/dataPreprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/dataPreprocessing.py
@@ -11,6 +11,7 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 #Taking care of missing data
+#Purpose: provide clearer data set for machine to parse
 from sklearn.preprocessing import Imputer
 #Use the mean of other rows data to fill in empty data
 imputer = Imputer(missing_values = 'NaN', strategy = "mean", axis = 0)
@@ -18,6 +19,7 @@ imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:, 1:3]) 
 
 #Encoding categorical data
+#Purpose: break data down so it can be understood by the machine
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 #break down first column to numerical values representing categories
@@ -29,3 +31,10 @@ x = onehotencoder.fit_transform(x).toarray()
 #break down labels into numerical values
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+#Splitting the dataset into the Training set and Test set
+#Purpose: train the machine learning algorithm and 
+#then test the preprocessing code on other data set to confirm
+#that it can dynamically preprocess data passed its way
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
